38b9e598f65d3625afb446a078709186b86e0a29
Shohnazar2003 <129948756+Shohnazar2003@users.noreply.github.com>
2023-08-20 04:25:44 +0500

    first commit

---
+++ script_1.js
@@ -0,0 +1,205 @@
+//  Data Types
+//  50 Example 
+
+// JavaScript has 8 Datatypes
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+
+// String 
+
+
+// 1
+
+// const name = 'Shohnazar';
+// const name1 = "Nizom";
+// const result = `The names are ${name} and ${name1}`;
+// console.log(result);
+
+
+// 2
+
+// let a = 'hello';
+// a = 'Hello';
+// console.log(a); // "Hello"
+
+// 3
+
+// const a = 'hello';
+// console.log(a.length); // 5
+
+
+// 4
+
+// const a = 'hello';
+// const b = new String('hello');
+
+// console.log(a); // "hello"
+// console.log(b); // "hello"
+
+// console.log(typeof a); // "string"
+// console.log(typeof b); // "object"
+
+
+// 5
+
+// const text1 = 'hello';
+// const text2 = 'world';
+// const text3 = '     JavaScript    ';
+
+// // concatenating two strings
+// const result1 = text1.concat(' ', text2);
+// console.log(result1); // "hello world"
+
+// // converting the text to uppercase
+// const result2 = text1.toUpperCase();
+// console.log(result2); // HELLO
+
+// // removing whitespace from the string
+// const result3 = text3.trim();
+// console.log(result3); // JavaScript
+
+// // converting the string to an array
+// const result4 = text1.split();
+// console.log(result4); // ["hello"]
+
+// // slicing the string
+// const result5= text1.slice(1, 3);
+// console.log(result5); // "el"
+
+
+// 6
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(7, 13);
+// console.log(part);
+
+
+// Number
+
+
+// 7
+
+// let y = 123e-5;
+// console.log(y);
+
+// 8
+
+// let num = 071 ;
+// console.log(num);
+
+// 9
+
+
+
+// 10
+
+// 11
+
+// 12
+
+
+// Bigint
+
+
+// 13
+
+// 14
+
+// 15
+
+// 16
+
+// 17
+
+// 18
+
+
+// Boolean
+
+
+// 19
+
+// 20
+
+// 21
+
+// 22
+
+// 23
+
+// 24
+
+
+// Undefined
+
+
+// 25
+
+// 26
+
+// 27
+
+// 28
+
+// 29
+
+// 30
+
+
+// Null
+
+
+// 31
+
+// 32
+
+// 33
+
+// 34
+
+// 35
+
+// 36
+
+
+// Symbol
+
+
+// 37
+
+// 38
+
+// 39
+
+// 40
+
+// 41
+
+// 42
+
+
+// Object
+
+
+// 43
+
+// 44
+
+// 45
+
+// 46
+
+// 47
+
+// 48
+
+// 49
+
+// 50
